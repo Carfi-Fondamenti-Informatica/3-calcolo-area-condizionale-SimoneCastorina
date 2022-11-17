02fd52f7102a402aad061977ba289610d9872a13
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-11-17 21:38:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
+   float a,b;
+    int opzione;
+    cin>>a>>b>>opzione;
+    switch(opzione){
+        case 1:
+         if(opzione==0){
+             cout<<(a*b)/2<<endl;
+             break;
+         }
+        case 2:
+         if(opzione==1) {
+             cout << a * a << endl;
+             break;
+         }
+        case 3:
+         if(opzione==2){
+             cout<<a*b<<endl;
+             break;
+         }
+
+        default:
+         cout<<"opzione non valida"<<endl;
+    }
    return 0;
 }
